d7738d7bc928f4b51c8892925aa2efd758dcb11e
JaishalPatel <80263421+JaishalPatel@users.noreply.github.com>
2021-06-04 14:43:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+    backgroundImg = loadImage(bg);
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+
+    var response = await fetch("");
+
+    //change the data in JSON format
+
+    hou = datetime.slice(11,13);
+
+
+    // add conditions to change the background images from sunrise to sunset
+
+
+    backgroundImg = loadImage(bg);
+
+}
